651fd5570d8bd6896605f3eefb6b24fc183c09a3
ChuishenX <chuishenx@oblivionocean.top>
2024-06-28 17:21:59 +0800

    Use English instead of Chinese; Added Option to install Java 6; Exceptions

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def java_install(ver, add_path=False):
         path = os.path.expanduser("~") + "\\.jdk"
         java_path = os.path.join(path, os.listdir(path)[0])
         if add_path:
-            os.system(f'chcp 65001&setx JAVA_HOME {java_path}&setx "Path" "%Path%;%JAVA_HOME%\\bin" /m')
+            os.system(f'chcp 65001 & setx JAVA_HOME {java_path} & setx "Path" "%Path%;%JAVA_HOME%\\bin" /m')
         return 0
     except Exception as e:
-        return -1
+        return e
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,15 +22,16 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.install_java)
-        self.ui.comboBox.addItems(["8", "11", "17","21"])
+        self.ui.comboBox.addItems(["6", "8", "11", "17", "21"])
     
     def install_java(self):
         ver = self.ui.comboBox.currentText()
         add_path = True
-        if java_install(ver, add_path) == 0:
-            QMessageBox.information(self, "提示", "安装成功", QMessageBox.Yes)
+        error = java_install(ver, add_path)
+        if error == 0:
+            QMessageBox.information(self, "Success", "Installation Complete.", QMessageBox.Yes)
         else:
-            QMessageBox.critical(self, "错误", "安装失败", QMessageBox.Yes)
+            QMessageBox.critical(self, "Error", "Unable to install.\n{error}", QMessageBox.Yes)
     
 
 if __name__ == '__main__':
